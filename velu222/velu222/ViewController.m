92c6f16002899e329c15bc24c5c9b7664ab96012
aswin <mobileteam@Aswin-Mac-mini.local>
2014-09-04 18:30:39 +0530

    Comments

--- velu222/velu222/ViewController.m
+++ velu222/velu222/ViewController.m
@@ -29,6 +29,8 @@
     
     NSLog(@"Text Changed Here!!!!");
     NSLog(@"jayavelu!!!");
+    
+    NSLog(@"dasdasdadasdasdadas");
 
 	// Do any additional setup after loading the view, typically from a nib.
 }
