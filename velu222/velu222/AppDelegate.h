3120848776a99bd2153eb4c1230d63e504a0774e
Jayavelu <jayaveluraj@gmail.com>
2014-09-04 17:27:59 +0530

    init commit

---
+++ velu222/velu222/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  velu222
+//
+//  Created by Jayavelu on 9/4/14.
+//  Copyright (c) 2014 Jayavelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ velu222/velu222/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  velu222
+//
+//  Created by Jayavelu on 9/4/14.
+//  Copyright (c) 2014 Jayavelu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSLog(@"Text Changed Here!!!!");
+    NSLog(@"Text Changed Here!!!!");
+    NSLog(@"Text Changed Here!!!!");
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
